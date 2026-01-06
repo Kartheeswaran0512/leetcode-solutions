94142dbcbf775907620c5bfa0a955ec03e81d5e9
Kartheeswaran0512 <88021315+Kartheeswaran0512@users.noreply.github.com>
2026-01-06 14:34:34 +0530

    Enhance performance analysis in solution comments
    Added comments and analysis regarding the performance of the solution for counting unequal triplets in an array.

--- 2566_number_of_unequal_triplets_in_array.cpp
+++ 2566_number_of_unequal_triplets_in_array.cpp
@@ -1,3 +1,28 @@
+nums.length ≤ 100
+3 loops
+→ 100 × 100 × 100 = 1,000,000
+→ runs instantly
+
+nums.length ≤ 10^5 
+optimized solution
+
+| Loops   | Max operations |
+| ------- | -------------- |
+| 1 loop  | 100            |
+| 2 loops | 10,000         |
+| 3 loops | 1,000,000      |
+| 4 loops | 100,000,000 ❌  |
+
+Step 3️⃣ Compare with computer limit
+
+Safe limit ≈ 10 million operations
+
+1,000,000 < 10,000,000 ✅
+
+
+So 3 loops is OK
+
+code :
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
