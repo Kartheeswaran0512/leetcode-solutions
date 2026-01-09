25affa878dcca750d0cede280bb52815d3a593ed
Kartheeswaran0512 <88021315+Kartheeswaran0512@users.noreply.github.com>
2026-01-09 13:10:53 +0530

    Add initial implementation of reversePrefix function

--- 4177_reverse_string_prefix.cpp
+++ 4177_reverse_string_prefix.cpp
@@ -1,3 +1,14 @@
+k/2 - how many swaps
+k-i-1 -  matching index from end
+i - start from beginning
+🔥 Interview Insight (VERY IMPORTANT)
+
+All these problems use ONE CORE IDEA:
+Left index  = i
+Right index = (start + length - 1) - i
+Swaps       = length / 2
+
+code:
 class Solution {
 public:
     string reversePrefix(string s, int k) {
